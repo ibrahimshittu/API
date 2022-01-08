fb647117aaa2f144cf14f35d45b3c6fcae83928b
Ibrahim Shittu <ibshittu01@gmail.com>
2022-01-08 11:40:41 +0100

    JWT refresh token enabled

--- incomexpensesapi/authentication/urls.py
+++ incomexpensesapi/authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import registerView, VerifyEmail, loginview
 
 urlpatterns = [
     path('register/', registerView.as_view(), name="register"),
     path('login/', loginview.as_view(), name="login"),
     path('verify-email/', VerifyEmail.as_view(), name="verify-email"),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
