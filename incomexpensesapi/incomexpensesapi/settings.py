3084b729be9ac8eadcbb46001c9f9d34faf810eb
Ibrahim Shittu <ibshittu01@gmail.com>
2022-01-09 17:45:16 +0100

    CORS Header

--- incomexpensesapi/incomexpensesapi/settings.py
+++ incomexpensesapi/incomexpensesapi/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'expenses',
     'income',
     'drf_yasg',
+    "corsheaders",
 ]
 
 SWAGGER_SETTINGS = {
@@ -65,7 +66,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS: True
 
 ROOT_URLCONF = 'incomexpensesapi.urls'
 
